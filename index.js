ef23b0fe7f9fa11f30b7d296a5123ec021819877
Rory Flint <rory@roryflint.co.uk>
2019-11-26 07:11:00 +0000

    Simple index.js created to test working functionality

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 require('dotenv').config();
+const express = require('express');
 
+const server = express();
+
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}.`);
+});
