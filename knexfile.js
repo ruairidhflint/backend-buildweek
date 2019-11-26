8f8f9d3a6b463b445e0b6de117a8f9bcf5588786
Rory Flint <rory@roryflint.co.uk>
2019-11-26 08:21:16 +0000

    Added variable to dbConfig to control whether production or development is used

--- knexfile.js
+++ knexfile.js
@@ -17,4 +17,14 @@ module.exports = {
       directory: './database/seeds',
     },
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
 };
